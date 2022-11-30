2a65d46053de4f849303f71e26f4bf5dd61bd21e
kgh128 <hayoung0627@gmail.com>
2022-11-30 17:42:29 +0900

    Add Setting LightValue 65

--- control_light/control_light.ino
+++ control_light/control_light.ino
@@ -1,6 +1,7 @@
 #include <SoftwareSerial.h>
 #include "GodoxRemote.h"
 
+
 // 핀 세팅
 const int pinTx = 8;  // 블루투스 TX 연결 핀 번호
 const int pinRx = 7;  // 블루투스 RX 연결 핀 번호
@@ -10,21 +11,23 @@ SoftwareSerial bluetooth(pinTx, pinRx); // 블루투스 송수신 핀 설정
 GodoxRemote remote(pinRF);              // RF 송신 핀 설정
 
 // 조명 밝기 세팅
-int lightValue = 25;
+int lightValue = 65;
+
+// 블루투스 수신 데이터
+String rxDataString = "";
 
 
 // 조명 밝기 Down 함수
 void lightDown() {
   // 조명 밝기가 25% 이하일 때는 더이상 밝기 Down 불가능
   if (lightValue <= 25) {
-    bluetooth.println(25);
+    bluetooth_write(25);
     return;
   }
 
   Serial.println("Down");
   remote.setBrightness(--lightValue);
-  bluetooth.println(lightValue);
-  delay(50);
+  bluetooth_write(lightValue);
   return;
 }
 
@@ -32,15 +35,21 @@ void lightDown() {
 void lightUp() {
   // 조명 밝기가 100% 이상일 때는 더이상 밝기 Up 불가능
   if (lightValue >= 100) {
-    bluetooth.println(100);
+    bluetooth_write(100);
     return;
   }
 
   Serial.println("Up");
   remote.setBrightness(++lightValue);
-  bluetooth.println(lightValue);
-  delay(50);
+  bluetooth_write(lightValue);
   return;
+}
+
+// 블루투스 전송 함수
+void bluetooth_write(int p_lightValue) {
+  String txData = String(p_lightValue);
+  txData += '\r';
+  bluetooth.println(txData);
 }
 
 
@@ -61,21 +70,30 @@ void setup() {
 }
 
 void loop() {
-  char rxData;  // 블루투스 수신 데이터
+  while (bluetooth.available()) {
+    char rxData = char(bluetooth.read());  // 블루투스 데이터 수신
 
-  if (bluetooth.available()) {
-    rxData = char(bluetooth.read());  // 블루투스 데이터 수신
-    
+    if (rxData == 'c') {
+      rxDataString = "";
+      continue;
+    }
+
+    rxDataString += rxData;
+
+    // 65를 수신하면 조명 밝기 65%로 초기화
+    if (rxDataString == "65") {
+      lightValue = 65;
+      remote.setBrightness(lightValue);
+    }
     // 0을 수신하면 밝기 Down
-    if (rxData == '0') {
+    if (rxDataString == "0") {
       lightDown();
     }
     // 1을 수신하면 밝기 Up
-    if (rxData == '1') { 
+    if (rxDataString == "1") { 
       lightUp();
     }
-  }
-  else {
+
     delay(10);
   }
 }
