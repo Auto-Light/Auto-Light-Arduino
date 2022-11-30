f3a69afbedc1b865de5282f276bbf7fcc81bde2d
kgh128 <hayoung0627@gmail.com>
2022-11-30 18:14:30 +0900

    Refactor Code

--- control_light/control_light.ino
+++ control_light/control_light.ino
@@ -10,7 +10,7 @@ const int pinRF = 4;  // RF 송신기 연결 핀 번호
 SoftwareSerial bluetooth(pinTx, pinRx); // 블루투스 송수신 핀 설정
 GodoxRemote remote(pinRF);              // RF 송신 핀 설정
 
-// 조명 밝기 세팅
+// 초기 조명 밝기
 int lightValue = 65;
 
 // 블루투스 수신 데이터
@@ -25,7 +25,6 @@ void lightDown() {
     return;
   }
 
-  Serial.println("Down");
   remote.setBrightness(--lightValue);
   bluetooth_write(lightValue);
   return;
@@ -39,7 +38,6 @@ void lightUp() {
     return;
   }
 
-  Serial.println("Up");
   remote.setBrightness(++lightValue);
   bluetooth_write(lightValue);
   return;
@@ -47,9 +45,9 @@ void lightUp() {
 
 // 블루투스 전송 함수
 void bluetooth_write(int p_lightValue) {
-  String txData = String(p_lightValue);
-  txData += '\r';
-  bluetooth.println(txData);
+  String txDataString = String(p_lightValue);
+  txDataString += '\r';
+  bluetooth.println(txDataString);
 }
 
 
@@ -73,6 +71,7 @@ void loop() {
   while (bluetooth.available()) {
     char rxData = char(bluetooth.read());  // 블루투스 데이터 수신
 
+    // 블루투스 수신 종료 코드
     if (rxData == 'c') {
       rxDataString = "";
       continue;
@@ -84,16 +83,23 @@ void loop() {
     if (rxDataString == "65") {
       lightValue = 65;
       remote.setBrightness(lightValue);
+      Serial.println(lightValue);
+      delay(10);
+      continue;
     }
-    // 0을 수신하면 밝기 Down
+    // 0을 수신하면 밝기 1% Down
     if (rxDataString == "0") {
       lightDown();
+      Serial.println(lightValue);
+      delay(10);
+      continue;
     }
-    // 1을 수신하면 밝기 Up
+    // 1을 수신하면 밝기 1% Up
     if (rxDataString == "1") { 
       lightUp();
+      Serial.println(lightValue);
+      delay(10);
+      continue;
     }
-
-    delay(10);
   }
 }
